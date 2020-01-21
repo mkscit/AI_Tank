aa4f7be0bb199317108ef0bc61477d3bb238b812
Mohammed Abushawish <59448589+mkscit@users.noreply.github.com>
2020-01-21 20:12:12 +0200

    NavMovementCompoennt

---
+++ Source/AI_Tank/AINavMovementComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/NavMovementComponent.h"
+#include "AINavMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AI_TANK_API UAINavMovementComponent : public UNavMovementComponent
+{
+	GENERATED_BODY()
+	
+};
